4264ec5e17d092f3f36eee36a80d45fa59857bdf
bear <aworkraf@gmail.com>
2022-11-22 17:14:42 +0100

    added loading progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,32 @@ import { ThemeProvider } from "theme-ui";
 import theme from "../../theme";
 import { WalletSelectorContextProvider } from "../contexts/WalletSelector";
 
+import "nprogress/nprogress.css";
+
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import { useEffect } from "react";
+
+
+
 function MyApp({ Component, pageProps }: AppProps) {
+  
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    Router.events.on("routeChangeStart", handleRouteStart);
+    Router.events.on("routeChangeComplete", handleRouteDone);
+    Router.events.on("routeChangeError", handleRouteDone);
+
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      Router.events.off("routeChangeStart", handleRouteStart);
+      Router.events.off("routeChangeComplete", handleRouteDone);
+      Router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, []);
+
   return (
     // @ts-ignore
     <ThemeProvider theme={theme}>
