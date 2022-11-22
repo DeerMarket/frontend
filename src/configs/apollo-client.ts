51529135a075b2622537b91626d52f56931ecaa4
bear <aworkraf@gmail.com>
2022-11-22 21:40:19 +0100

    fix cache

--- src/configs/apollo-client.ts
+++ src/configs/apollo-client.ts
@@ -1,8 +1,14 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+const uri =
+  process.env.NODE_ENV === "development"
+    ? "https://api.thegraph.com/subgraphs/name/dino-bear/deertest"
+    : "https://api.thegraph.com/subgraphs/name/dino-bear/deer";
 const client = new ApolloClient({
-  uri: "https://api.thegraph.com/subgraphs/name/dino-bear/deer",
-  cache: new InMemoryCache(),
+  uri: uri,
+  cache: new InMemoryCache({
+    resultCaching: false,
+  }),
 });
 
 export default client;
